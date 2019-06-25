1e26701b7438d39aff830264c3475ff621edd9f3
Joan Sánchez <joansvich@gmail.com>
2019-06-25 11:04:06 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from './components/Error';
 
 function App() {
+
+  // State principal
+  // ciudad = state, guardarCiudad = this.setState();
+  const [ciudad, guardarCiudad] = useState('');
+  const [pais, guardarPais] = useState('');
+  const [error, guardarError] = useState(false);
+
+
+  const datosConsulta = datos => {
+
+    // Validación
+    if (!datos.ciudad || !datos.pais) {
+      guardarError(true);
+      return;
+    }
+
+    // agregarlos al state
+    guardarCiudad(datos.ciudad);
+    guardarPais(datos.pais);
+    guardarError(false);
+  }
+
+  // Cargar componente condicionalmente
+
+  let componente;
+
+  if (error) {
+    componente = <Error mensaje='Ambos campos son obligatorios' />
+  } else {
+    componente = null;
+  }
+
+
   return (
-    <h1>Hola</h1>
+    <div className="App">
+      <Header
+        titulo='Clima React App'
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario
+                datosConsulta={datosConsulta}
+              />
+            </div>
+
+            <div className="col s12 m6">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
